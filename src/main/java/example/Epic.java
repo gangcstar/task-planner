32e6e82c113463a69c6f839eb1d4e2fc83b83dfc
Andrew <gangcstar@users.noreply.github.com>
2025-04-17 00:56:39 +0400

    написал класс Epic

--- src/main/java/example/Epic.java
+++ src/main/java/example/Epic.java
@@ -1,4 +1,26 @@
 package example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Epic extends Task {
+    private List<Integer> subTaskIds;
+
+    public Epic(int id, String name, String description) {
+        super(id, name, description, Status.NEW);
+        this.subTaskIds = new ArrayList<>();
+    }
+
+    public List<Integer> getSubTaskIds() {return subTaskIds}
+
+    public void addSubTaskId(int subTaskId) {
+        subTaskIds.add(subTaskId);
+    }
+
+    public void removeSubTaskId(int subTaskId) {
+        subTaskIds.remove(Integer.valueOf(subTaskId));
+    }
+
+
+
 }
