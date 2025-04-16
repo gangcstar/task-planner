8bf98243728e294db32a38919865eb0ffd477792
Andrew <gangcstar@users.noreply.github.com>
2025-04-17 00:38:37 +0400

    написал класс SubTask

--- src/main/java/example/Subtask.java
+++ src/main/java/example/Subtask.java
@@ -1,4 +1,12 @@
 package example;
 
 public class Subtask extends Task {
+    private int epicId;
+
+    public Subtask(int id, String name, String description, Status status, int epicId) {
+        super(id, name, description,status);
+        this.epicId = epicId;
+    }
+
+    public int getEpicId() {return epicId;}
 }
