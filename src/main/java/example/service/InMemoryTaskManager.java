ebb4c8867c680a3259811ec2bd9ca5ba715fb2f3
Andrew <gangcstar@users.noreply.github.com>
2025-05-15 00:43:15 +0400

    обновил данные в интерфейсе. TaskManager переименован в InMemoryTaskManager;

--- src/main/java/example/service/InMemoryTaskManager.java
+++ src/main/java/example/service/InMemoryTaskManager.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class TaskManagerImpl<T extends Task> implements TaskManager<T> {
+public class InMemoryTaskManager<T extends Task> implements TaskManager<T> {
     private final Map<Integer, T> tasks = new HashMap<>();
 
     @Override
--- src/main/java/example/service/TaskManager.java
+++ src/main/java/example/service/TaskManager.java
@@ -1,10 +1,24 @@
 package example.service;
 
+import example.model.Epic;
+import example.model.Subtask;
 import example.model.Task;
+
+import java.util.List;
 
 public interface TaskManager<T extends Task> {
 
     void add(T task);
 
     void remove(int id);
+
+    public List<Task> getAllTasks();
+
+    public List<Epic> getAllEpics();
+
+    public List<Subtask> getAllSubtasks();
+
+    public List<Subtask> getSubtasksOfEpic(int epicId);
+
 }
+
